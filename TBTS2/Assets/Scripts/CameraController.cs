fc7a1b8c22dc28841e26b5e98e975e93d8b21cec
Matteo Pavesi <matteo.pavesi@gamesys.co.uk>
2019-07-21 00:05:38 +0100

    moving camera

---
+++ TBTS2/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.position = new Vector3(3, 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject target = this.gameController.GetCurrentPlayer();
+        this.transform.position = target.transform.position + new Vector3(3, 3, 3);
+        this.transform.LookAt(target.transform);
+    }
+}
--- TBTS2/Assets/Scripts/GameController.cs
+++ TBTS2/Assets/Scripts/GameController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject currentPlayer;
+
     void Start()
     {
         GameObjectFactory gameObjectFactory = gameObject.AddComponent<GameObjectFactory>();
         new TileSet("TileSet", 9, 9, Vector3.zero, gameObjectFactory).Create();
         new TileSet("TileSet2", 7, 7, new Vector3(12,0,0), gameObjectFactory).Create();
-        gameObjectFactory.Resource("Player/Capsule").Position(0, 1.05f, 0).Create();
+        this.currentPlayer = gameObjectFactory.Resource("Player/Capsule").Position(0, 1.05f, 0).Create();
     }
 
-    // Update is called once per frame
+    public GameObject GetCurrentPlayer()
+    {
+        return this.currentPlayer;
+    }
+
     void Update()
     {
         
