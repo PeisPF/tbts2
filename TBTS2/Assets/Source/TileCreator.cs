db0219b1254a1af69e204016abae0bb392c5e735
Matteo Pavesi <matteo.pavesi@gamesys.co.uk>
2019-07-20 19:58:02 +0100

    Script

---
+++ TBTS2/Assets/Source/TileCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TileCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Object tileObject = Resources.Load("Tiles/Tile");
+        GameObject tile = (UnityEngine.GameObject) Instantiate(tileObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
