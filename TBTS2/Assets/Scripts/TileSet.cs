9da9beb6139d76de4f5744faaacdb83ca025490a
Matteo Pavesi <matteo.pavesi@gamesys.co.uk>
2019-07-23 20:44:29 +0100

    Adjacency not working

--- TBTS2/Assets/Scripts/TileSet.cs
+++ TBTS2/Assets/Scripts/TileSet.cs
@@ -10,6 +10,7 @@ public class TileSet
     private int sizeZ;
     private string name;
     private GameObjectFactory gameObjectFactory;
+    private List<GameObject> tiles = new List<GameObject>();
 
     public TileSet(string name, int sizeX, int sizeZ, Vector3 center, GameObjectFactory gameObjectFactory)
     {
@@ -30,13 +31,22 @@ public class TileSet
         int startZ = (int)center.z - Mathf.FloorToInt(sizeZ / 2);
         int endZ = startZ + sizeZ;
         for (int x=startX; x < endX; x++){
-            for (int z=startZ; z < endZ; z++){
-                gameObjectFactory.Resource("Tiles/Tile").Parent(tileSet.transform).Position(x,z).Create();
+            for (int z=startZ; z < endZ; z++)
+            {
+                GameObject tile = CreateTile(tileSet, x, z);
+                tiles.Add(tile);
             }
         }
         return this;      
     }
 
+    public List<GameObject> GetTiles()
+    {
+        return tiles;
+    }
 
-
+    private GameObject CreateTile(GameObject tileSet, Int32 x, Int32 z)
+    {
+        return gameObjectFactory.Resource("Tiles/Tile").Parent(tileSet.transform).Position(x, z).Create();
+    }
 }
